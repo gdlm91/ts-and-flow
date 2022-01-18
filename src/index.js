e6623d34d6fe5e7f5214c51adee0bdee591ab15d
Gerardo Leal <gleal@atlassian.com>
2022-01-18 13:54:54 +1100

    Webpack + Babel config for React

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const title = "React with Webpack, Flow and Babel";
+
+ReactDOM.render(<App title={title} />, document.getElementById("app"));
